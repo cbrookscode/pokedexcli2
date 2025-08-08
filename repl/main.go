36d3aa362e75ea972a93ece7737b4025ae8c0b20
cbrookscode <colbybrooks@myyahoo.com>
2025-08-08 13:04:12 -0400

    added some additional comments in main.go

--- repl/main.go
+++ repl/main.go
@@ -7,24 +7,30 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	// get available commands for the program
 	availableCommands := registerCommands()
+
+	// listen for user input
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
 
+		// advance scanner to next token
 		ready := scanner.Scan()
 		if !ready {
 			fmt.Printf("Error scanning for user input: %v", scanner.Err())
 			continue
 		}
 
+		// grab user input from scanner in string format
 		input := scanner.Text()
 		cleaned := cleanInput(input)
 		if len(cleaned) == 0 {
 			fmt.Print("No command provided\n")
 			continue
 		}
+		// command for cli will always be first word in user input
 		usersCommand := cleaned[0]
 
 		command, ok := availableCommands[usersCommand]
