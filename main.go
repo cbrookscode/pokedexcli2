d6188fe428d298b7a674ced53aa023299baa44d8
cbrookscode <colbybrooks@myyahoo.com>
2025-08-26 11:12:41 -0400

    finished initial stages of custom input handler to accomodate historic pagination of past inputs and more standard terminal features while in cli

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/cbrookscode/pokedexcli2/internal"
 	"github.com/cbrookscode/pokedexcli2/repl"
 )
 
 func main() {
 	// get available commands for the program
-	// availableCommands := repl.RegisterCommands()
+	availableCommands := repl.RegisterCommands()
 
-	// cache := internal.NewCache(60 * time.Second)
+	cache := internal.NewCache(60 * time.Second)
 
 	fd := os.Stdin.Fd()
 	orig, err := repl.EnableRawMode(fd)
@@ -21,105 +23,34 @@ func main() {
 	}
 	defer repl.DisableRawMode(fd, orig)
 
-	// config := repl.Config{Next: "", Current: "", Previous: nil, Orig_Term_Settings: orig}
+	term := repl.Terminal{
+		History: []string{},
+	}
 
-	// pokedex := internal.Pokedex{Entries: make(map[string]internal.Pokemon)}
+	config := repl.Config{Next: "", Current: "", Previous: nil, Orig_Term_Settings: orig}
 
-	// listen for user input
-	// scanner := bufio.NewScanner(os.Stdin)
+	pokedex := internal.Pokedex{Entries: make(map[string]internal.Pokemon)}
 
-	history := []string{}
-	count := 1
-	input_bytes := []byte{}
-	fmt.Print("Pokedex > ")
 	for {
+		repl.HandleUserInput(&term)
 
-		buf := make([]byte, 1)
-
-		_, err := os.Stdin.Read(buf)
-		if err != nil {
-			fmt.Printf("error reading user input\n")
-			os.Exit(2)
+		cleaned := repl.CleanInput(term.User_input)
+		if len(cleaned) == 0 {
+			continue
 		}
-		switch buf[0] {
-		case '\r', '\n':
-			fmt.Print("\n")
-			fmt.Print("Pokedex > ")
-			history = append(history, string(input_bytes))
-			count = 1
-			input_bytes = input_bytes[:0]
-		case '\b', '\x7F':
-			if len(input_bytes) == 0 {
-				continue
-			}
-			input_bytes = input_bytes[:len(input_bytes)-1]
-			fmt.Print("\b \b")
-		case '\x1b':
-			_, err = os.Stdin.Read(buf)
-			if err != nil {
-				fmt.Printf("error reading user input\n")
-				os.Exit(2)
-			}
-			switch buf[0] {
-			case '[':
-				os.Stdin.Read(buf)
-				switch buf[0] {
-				case 'A':
-					if len(history) < 1 || len(history)-count < 0 {
-						continue
-					}
-					fmt.Print("\r\033[2K")
-					fmt.Print("Pokedex > ")
-					previous_input := history[len(history)-count]
-					fmt.Printf("%s", previous_input)
-					input_bytes = []byte(previous_input)
-					count++
-				case 'B':
-					count--
-					if len(history) < 1 || len(history)-count > len(history) {
-						count++
-						continue
-					}
-					fmt.Print("\r\033[2K")
-					fmt.Print("Pokedex > ")
-					previous_input := history[len(history)-count]
-					fmt.Printf("%s", previous_input)
-					input_bytes = []byte(previous_input)
-				case 'C':
-				case 'D':
-				}
-			}
-		default:
-			input_bytes = append(input_bytes, buf[0])
-			fmt.Printf("%c", buf[0])
+
+		// command for cli will always be first word in user input
+		usersCommand := cleaned[0]
+
+		command, ok := availableCommands[usersCommand]
+		if !ok {
+			fmt.Printf("Command provided(%v) does not exist\n", usersCommand)
+			continue
+		}
+		err = command.Callback(&config, cache, &pokedex, term.User_input)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 	}
 }
-
-// advance scanner to next token
-// ready := scanner.Scan()
-// if !ready {
-// 	fmt.Printf("Error scanning for user input: %v", scanner.Err())
-// 	continue
-// }
-
-// grab user input from scanner in string format
-// input := scanner.Text()
-// cleaned := repl.CleanInput(input)
-// if len(cleaned) == 0 {
-// 	fmt.Print("No command provided\n")
-// 	continue
-// }
-// command for cli will always be first word in user input
-// usersCommand := cleaned[0]
-
-// command, ok := availableCommands[usersCommand]
-// if !ok {
-// 	fmt.Printf("Command provided(%v) does not exist\n\n", usersCommand)
-// 	continue
-// }
-// err := command.Callback(&config, cache, &pokedex, input)
-// if err != nil {
-// 	fmt.Println(err)
-// 	return
-// }
--- repl/repl.go
+++ repl/repl.go
@@ -1,11 +1,98 @@
 package repl
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"syscall"
 	"unicode"
 	"unsafe"
 )
+
+type Terminal struct {
+	Input_bytes []byte
+	Index       int
+	History     []string
+	User_input  string
+}
+
+func HandleUserInput(term *Terminal) {
+	exit := false
+
+	fmt.Print("Pokedex > ")
+	for {
+		buf := make([]byte, 1)
+
+		_, err := os.Stdin.Read(buf)
+		if err != nil {
+			fmt.Printf("error reading user input\n")
+			os.Exit(2)
+		}
+		switch buf[0] {
+		// enter
+		case '\r', '\n':
+			fmt.Print("\n")
+			term.History = append(term.History, string(term.Input_bytes))
+			term.Index = len(term.History)
+			term.User_input = string(term.Input_bytes)
+			term.Input_bytes = term.Input_bytes[:0]
+			exit = true
+		// backspace or del
+		case '\b', '\x7F':
+			if len(term.Input_bytes) == 0 {
+				continue
+			}
+			term.Input_bytes = term.Input_bytes[:len(term.Input_bytes)-1]
+			fmt.Print("\b \b")
+		// escape key / start of arrow keys
+		case '\x1b':
+			_, err = os.Stdin.Read(buf)
+			if err != nil {
+				fmt.Printf("error reading user input\n")
+				os.Exit(2)
+			}
+			switch buf[0] {
+			case '[':
+				os.Stdin.Read(buf)
+				switch buf[0] {
+				case 'A':
+					if len(term.History) < 1 {
+						continue
+					}
+					if term.Index-1 >= 0 {
+						term.Index--
+						fmt.Print("\r\033[2K")
+						fmt.Print("Pokedex > ")
+						previous_input := term.History[term.Index]
+						fmt.Printf("%s", previous_input)
+						term.Input_bytes = []byte(previous_input)
+					}
+				case 'B':
+					if len(term.History) < 1 {
+						continue
+					}
+					if term.Index+1 >= len(term.History) {
+						continue
+					}
+					term.Index++
+					fmt.Print("\r\033[2K")
+					fmt.Print("Pokedex > ")
+					previous_input := term.History[term.Index]
+					fmt.Printf("%s", previous_input)
+					term.Input_bytes = []byte(previous_input)
+				case 'C':
+				case 'D':
+				}
+			}
+		default:
+			term.Input_bytes = append(term.Input_bytes, buf[0])
+			fmt.Printf("%c", buf[0])
+		}
+		if exit {
+			break
+		}
+	}
+}
 
 // passed fd can be obtained by os.stdin.fd(). returned termious struct pointer will be original terminal settings to revert back to
 func EnableRawMode(fd uintptr) (*syscall.Termios, error) {
